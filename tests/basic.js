9aeedfe9a02b86b23739ee4908e32781910125d2
Andreas Holstenson <a@holstenson.se>
2017-11-04 15:00:20 +0100

    Adding basic tests

---
+++ tests/basic.js
@@ -0,0 +1,112 @@
+const expect = require('chai').expect;
+const { toExtendable, Mixin, Class } = require('../');
+
+describe('Basic tests', () => {
+	it('Can make class extendable', () => {
+		const A = toExtendable(class {
+			a() {
+				return 1;
+			}
+		});
+
+		const instance = new A();
+		expect(instance.a()).to.equal(1);
+	});
+
+	it('Can create and apply mixin', () => {
+		const A = toExtendable(class {
+			a() {
+				return 1;
+			}
+		});
+
+		const B = Mixin(Base => class B extends Base {
+			b() {
+				return 2;
+			}
+		});
+
+		const C = A.with(B);
+		const instance = new C();
+		expect(instance.a()).to.equal(1);
+		expect(instance.b()).to.equal(2);
+	});
+
+	it('Mixin can call super', () => {
+		const A = toExtendable(class {
+			a() {
+				return 1;
+			}
+		});
+
+		const B = Mixin(Base => class B extends Base {
+			a() {
+				return 1 + super.a();
+			}
+		});
+
+		const C = A.with(B);
+		const instance = new C();
+		expect(instance.a()).to.equal(2);
+	});
+
+	it('Mixable Class can be created', () => {
+		const A = toExtendable(class {
+			a() {
+				return 1;
+			}
+		});
+
+		const B = Class(A, Base => class B extends Base {
+			a() {
+				return 1 + super.a();
+			}
+		});
+
+		const instance = new B();
+		expect(instance.a()).to.equal(2);
+	});
+
+	it('Mixable Class can also be mixed in', () => {
+		const A = toExtendable(class {
+			a() {
+				return 1;
+			}
+		});
+
+		const B = Class(A, Base => class B extends Base {
+			a() {
+				return 1 + super.a();
+			}
+		});
+
+		const C = A.with(B);
+		const instance = new C();
+		expect(instance.a()).to.equal(2);
+	});
+
+	it('Mixable Class can be extended', () => {
+		const A = toExtendable(class {
+			a() {
+				return 1;
+			}
+		});
+
+		const B = Class(A, Base => class B extends Base {
+			a() {
+				return 1 + super.a();
+			}
+		});
+
+		const C = Mixin(Base => class C extends Base {
+			b() {
+				return 1;
+			}
+		});
+
+		const D = B.with(C);
+		const instance = new D();
+		expect(instance.a()).to.equal(2);
+		expect(instance.b()).to.equal(1);
+	});
+});
